e8036270180aad9abf698867e85b85bed4f6bfd0
Nick Schonning <nschonni@gmail.com>
2012-11-04 00:38:44 -0400

    Bump version and minify
    Used http://refresh-sf.com/yui/ for minfication

--- jquery.details.js
+++ jquery.details.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/details v0.0.6 by @mathias | includes http://mths.be/noselect v1.0.3 */
+/*! http://mths.be/details v0.0.7 by @mathias | includes http://mths.be/noselect v1.0.3 */
 ;(function(document, $) {
 	"use strict";
 	var proto = $.fn,
